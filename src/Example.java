dce376910738ad8c6aa656dc6312afa43e9fd5b9
Guillermo Rodriguez <guillermo.rodriguez.utu@gmail.com>
2016-03-11 10:34:25 -0300

    Working in making update method that gets called every frame, this code causes extreme flickering, gona switch to javax.swing.Timer instead of using SwingWorker and diferent threads

--- src/Example.java
+++ src/Example.java
@@ -1,34 +1,61 @@
-import term.Terminal;
+import term.JTerminal;
 import java.awt.Color;
 import javax.swing.SwingWorker;
+import javax.swing.SwingUtilities;
+import java.awt.Point;
+import java.awt.MouseInfo;
+import java.awt.PointerInfo;
+import java.awt.image.BufferedImage;
 
 public class Example extends SwingWorker<Integer, Void> {
 
-	private Terminal jterm;
+	private JTerminal jterm;
 
 	public Example(){
 
-		jterm = new Terminal("test",120,40);
+		jterm = new JTerminal("test",120,40);
+		jterm.setLocation(100,100);
 		jterm.setVisible(true);
+		/*jterm.setCursor(jterm.getToolkit().createCustomCursor(
+            new BufferedImage(3, 3, BufferedImage.TYPE_INT_ARGB), new Point(0, 0),
+            "null"));*/
 		execute();
 	}
 
 	@Override
 	public Integer doInBackground(){
 
-		int i = 0;
-
-		while(!Terminal.k_0){
-
+		while(!JTerminal.k_0){
+			
 			try{
-				Thread.sleep(15);
-			}catch(InterruptedException e){
+
+				Thread.sleep(17);
+
+			} catch (InterruptedException e){
 
 
 
 			}
-			i++;
-			System.out.println("tick " + i);
+			jterm.clear();
+			/* MOUSE POS DEMO
+			Point mousePos = MouseInfo.getPointerInfo().getLocation();
+			SwingUtilities.convertPointFromScreen(mousePos,jterm);
+			mousePos.y -= 25;
+			Point charMousePos = jterm.screenToChar(mousePos);
+			jterm.setColor(true,charMousePos.x,charMousePos.y,Color.red);
+			jterm.setColor(false,charMousePos.x,charMousePos.y,Color.blue);
+			jterm.setChar('0',charMousePos.x,charMousePos.y);
+			*/
+			for(int x = 0; x < 120; x++){
+
+				for(int y = 0; y < 40; y++){
+
+					jterm.setColor(true,x,y,new Color(x/120f,x/120f,x/120f));
+				
+				}
+
+			}
+			jterm.render();
 
 		}
 
